d938bcc26a4dce6faeb23e55f5783b7b45e38e6d
frostie <mattwiner04@gmail.com>
2024-07-15 15:14:27 -0700

    48

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import db from "@astrojs/db";
+import markdoc from "@astrojs/markdoc";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()]
+  integrations: [db(), markdoc(), mdx(), tailwind(),
+  ]
 });
--- db/config.ts
+++ db/config.ts
@@ -1,11 +1,11 @@
 import { defineDb, defineTable, column } from 'astro:db';
 const Posts = defineTable({
   columns: {
-    title: column.text(),
+    id: column.number(),
+    name: column.text(),
     date: column.date(),
-    body: column.text(),
   }
-})
+});
 // https://astro.build/db/config
 export default defineDb({
   tables: { Posts },
