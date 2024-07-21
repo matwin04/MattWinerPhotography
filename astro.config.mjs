6a560fb2bd3749d3f28ffe34f28cb90ef61309eb
frostie <mattwiner04@gmail.com>
2024-07-21 11:27:17 -0700

    M50

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
 import markdoc from "@astrojs/markdoc";
 import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), markdoc(), mdx()]
+  integrations: [db(), markdoc(), mdx(), tailwind(),
+  ]
 });
