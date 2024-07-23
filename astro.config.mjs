bc7f807e88ed1f54569974f0291d540c75abbc3b
frostie <mattwiner04@gmail.com>
2024-07-22 22:10:22 -0700

    60

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
-import db from "@astrojs/db";
-import markdoc from "@astrojs/markdoc";
-import mdx from "@astrojs/mdx";
-
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [db(), markdoc(), mdx()]
-});
+export default defineConfig({});
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,1 @@
-/// <reference path="../.astro/db-types.d.ts" />
 /// <reference types="astro/client" />
